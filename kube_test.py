6a903cbf5f32beb5be28b6d2ba4f1622f640b0aa
Mike Borozdin <mikebz@google.com>
2025-04-01 10:31:48 -0700

    add test for MCP tools in kube_test.py

--- kube_test.py
+++ kube_test.py
@@ -2,6 +2,7 @@ import asyncio
 import pytest
 from kubernetes import config
 from generate import create_lister
+from kube import add_tools, mcp
 
 config.load_kube_config("~/.kube/config")
 
@@ -23,4 +24,6 @@ async def test_get_namespaces():
 def test_add_tools():
     """Test adding MCP tools by calling add_tools() and then
     checking that those actually exist."""
-    
+    add_tools()
+    assert len(mcp._tool_manager._tools) > 0
+    assert "list_namespace" in mcp._tool_manager._tools
