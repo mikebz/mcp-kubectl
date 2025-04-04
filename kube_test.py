4ae164e338d13932dd0fb1f0e62d1fb1696bf4fe
Mike Borozdin <mikebz@google.com>
2025-04-04 09:19:00 -0700

    Update CI workflow to exclude integration tests and enhance test configuration

--- kube_test.py
+++ kube_test.py
@@ -7,11 +7,11 @@ from kubernetes import config
 from generate import create_lister
 from kube import add_tools, mcp
 
-config.load_kube_config("~/.kube/config")
-
+@pytest.mark.integration
 @pytest.mark.asyncio
 async def test_get_namespaces():
     """Test the get_namespaces function."""
+    config.load_kube_config("~/.kube/config")
 
     method = create_lister( "list_namespace")
     # Ensure the function is callable
@@ -24,13 +24,18 @@ async def test_get_namespaces():
     assert isinstance(namespaces, list)
     assert len(namespaces) > 0
 
+@pytest.mark.integration
 @pytest.mark.asyncio
 async def test_add_tools():
     """Test adding MCP tools by calling add_tools() and then
     checking that those actually exist."""
+    config.load_kube_config("~/.kube/config")
+
     add_tools()
 
     tools = await mcp.list_tools()
     tools_length = len(tools)
+    names = [tool.name for tool in tools]
     assert tools_length > 0
-    assert "list_namespace" in tools
+    assert "list_namespace" in names
+    assert "list_event_for_all_namespaces" in names
